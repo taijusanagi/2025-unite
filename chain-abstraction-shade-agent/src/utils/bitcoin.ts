c2e2ac6bf1f4c840f852c7a6fba92c6a11e1aedc
taijusanagi <taijusanagi@gmail.com>
2025-08-03 13:18:02 +1200

    btc account works

--- chain-abstraction-shade-agent/src/utils/bitcoin.ts
+++ chain-abstraction-shade-agent/src/utils/bitcoin.ts
@@ -6,8 +6,10 @@ const MPC_CONTRACT = new contracts.ChainSignatureContract({
   contractId: `v1.signer-prod.testnet`,
 });
 
+// I use testnet3, so and mempool for testnet 3 fails to get balance
+// But I found blockstream works for get address and balanace, so I use it here
 const btcRpcAdapter = new chainAdapters.btc.BTCRpcAdapters.Mempool(
-  "https://mempool.space/testnet/api"
+  "https://blockstream.info/testnet/api"
 );
 
 export const Btc = new chainAdapters.btc.Bitcoin({
